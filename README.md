b160ade07401441e1c00a60aa0549d1c569da560
Roger-Wu <gsetcrw@gmail.com>
2017-03-16 03:41:26 +0800

    add sample_config.js, refactor app.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bitcoin-price-alert-bot
 A cryptocurrency price alert chat bot using Poloniex API and Facebook chat API
+
+## Getting started
+1. copy and rename `sample_config.js` to `config.js`
+2. edit config.js
+3. run `npm install` and `npm start`
--- app.js
+++ app.js
@@ -1,115 +1,95 @@
 'use strict';
 
 const login = require('facebook-chat-api');
-const bitcoinex = require('bitcoinex');
-const request = require('request');
-// const ggbMongo = require('./daemon/ggbMongo');
 const Poloniex = require('poloniex.js');
+
+const config = require('./config.js')
+
 const poloniex = new Poloniex();
-
-const config = require('./config.js');
-
-const THREAD_ID = config.thread_id; // Bitcoin chat box thread id.
-const EMAIL = config.email;
-const PASSWORD = config.password;
-// Create simple echo bot
-
-// const COMMAND_LIST = [
-//   '/addlaoshi',
-//   '/removelaoshi',
-//   '/listlaoshi',
-//   '/maicoin',
-//   '/eth',
-//   '/bitfinex',
-// ];
-
 let previousPrices;
 
-function priceAlert(api) {
-  poloniex.returnTicker((err, response) => {
+const bot = () => {
+  login({ email: config.email, password: config.password }, (err, api) => {
     if (err) {
-      console.log(`Error: prices not fetched. Retry in 5 seconds`);
-      setTimeout(() => priceAlert(api), 1000 * 5); // try again in 5 seconds
-    } else {
-      if (!previousPrices) {
-        console.log('initial prices fetched');
-        previousPrices = response;
-      } else {
-        console.log('prices fetched');
-        let text = '';
-        config.coin_pairs.map((coin_pair) => {
-          const currPrice = parseFloat(response[coin_pair].last);
-          const prevPrice = parseFloat(previousPrices[coin_pair].last);
-          const rate = (currPrice - prevPrice) / prevPrice;
-          if (Math.abs(rate) > config.alert_rate) {
-            text += `${coin_pair} ${config.alert_time_period}秒內從 ${prevPrice} ${currPrice > prevPrice ? '上升至' : '下降至'} ${currPrice} ，變化 ${(rate*100).toFixed(2)}%\n`;
-          }
-        });
-        if (text) {  // not empty
-          api.sendMessage(text, THREAD_ID);
-          console.log(`message sent: ${text}`);
-        }
-        previousPrices = response;
-      }
-      setTimeout(() => priceAlert(api), 1000 * config.alert_time_period);
-    }
-  });
-}
-
-const echo = () => {
-  login({ email: EMAIL, password: PASSWORD }, (err, api) => {
-    if (err) {
-      setInterval(echo, 1000 * 10);
+      setInterval(bot, 1000 * 10);
       return console.error(err);
     }
-
-    priceAlert(api);
+    sendInitialMessage(api);
+    monitorPrices(api);
 
     api.listen((listenError, message) => {
       if (!listenError) {
+        console.log(message);
         if (/\/a/.test(message.body)) {
-          poloniex.returnTicker((err, response) => {
-            if (err) {
-              // Log error message
-              console.log("An error occurred: " + err.msg);
-            } else {
-              // console.log(response);
-              let text = 'Poloniex 價格資訊：\n\n';
-              config.coin_pairs.map(coin_pair => {
-                text += `${coin_pair}: ${response[coin_pair].last} ${response[coin_pair].percentChange}\n`;
-              });
-              console.log(message.threadID);
-              api.sendMessage(text, message.threadID);
-            }
-          });
-        } else if (/\/bitfinex/.test(message.body)) {
-          bitcoinex.getPriceWith('bitfinex', 'usd', (getPriceError, result) => {
-            if (!getPriceError) {
-              const text = `Bitfinex 的價格資訊：\n\n當日最高價(high): ${result.high.toFixed(2)} USD\n當日最低價(low): ${result.low.toFixed(2)} USD\n最後成交價(last): ${result.last.toFixed(2)} USD`;
-              api.sendMessage(text, message.threadID);
-            } else {
-              console.error(getPriceError);
-            }
-          });
-        } else if (/\/eth/.test(message.body)) {
-          console.log('receive command: eth');
-          request('https://api.coinbase.com/v2/prices/ETH-USD/spot', (error, response, body) => {
-            if (error) console.error(`error:${error}`);
-            else {
-              const text = `Coinbase 的 ETH 最新成交價：${JSON.parse(body).data.amount} ${JSON.parse(body).data.currency}`;
-              api.sendMessage(text, message.threadID);
-            }
-          });
+          returnPrices(api, message);
         }
-        console.log(message, null, 4);
       }
     });
     return 0;
   });
 };
 
-echo();
+function sendInitialMessage(api) {
+  let text = `Hi! I'm price alert bot!\nI'm monitoring the prices of\n`;
+  config.alert_coin_pairs.map((coin_pair) => {
+    text += coin_pair + `\n`;
+  });
+  text += `and if a price changes over +- ${config.alert_rate * 100}% in ${config.alert_time_interval} seconds, I will tell you immediately!`
+
+  api.sendMessage(text, config.thread_id);
+}
+
+function monitorPrices(api) {
+  poloniex.returnTicker((err, response) => {
+    if (err) {
+      console.log(`Error: prices not fetched. Retry in 5 seconds`);
+      setTimeout(() => monitorPrices(api), 1000 * 5); // try again in 5 seconds
+    } else {
+      if (!previousPrices) {
+        console.log('initial prices fetched');
+        previousPrices = response;
+      } else {
+        console.log('prices fetched');
+        // for each coin pair, check if the rate of change is greater than the threshold
+        let text = '';
+        config.alert_coin_pairs.map((coin_pair) => {
+          const currPrice = parseFloat(response[coin_pair].last);
+          const prevPrice = parseFloat(previousPrices[coin_pair].last);
+          const rate = (currPrice - prevPrice) / prevPrice;
+          if (Math.abs(rate) > config.alert_rate) {
+            text += `Price of ${coin_pair} changes \nfrom ${prevPrice} \nto ${currPrice} in ${config.alert_time_interval} seconds.\nRate of change: ${(rate*100).toFixed(2)}%\n`;
+          }
+        });
+        // if text not empty, send the alert
+        if (text) {
+          api.sendMessage(text, config.thread_id);
+          console.log(`message sent: ${text}`);
+        }
+        previousPrices = response;
+      }
+      setTimeout(() => monitorPrices(api), 1000 * config.alert_time_interval);
+    }
+  });
+}
+
+function returnPrices(api, message) {
+  poloniex.returnTicker((err, response) => {
+    if (err) {
+      console.log(`Error: prices not fetched. Retry in 5 seconds`);
+      setTimeout(() => returnPrices(api, message), 1000 * 5); // try again in 5 seconds
+    } else {
+      let text = ``;
+      config.alert_coin_pairs.map(coin_pair => {
+        text += `${coin_pair}: ${response[coin_pair].last} ${response[coin_pair].percentChange}\n`;
+      });
+      console.log(message.threadID);
+      api.sendMessage(text, message.threadID);
+    }
+  });
+}
+
+bot();
 
 // module.exports = {
-//   echo,
+//   bot,
 // };
